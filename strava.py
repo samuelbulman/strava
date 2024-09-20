0b68c17b2fb1f028d0af438a5862756f7ef541a1
samuelbulman <samuelabulman@gmail.com>
2024-09-19 20:25:54 -0500

    build athletes table

--- strava.py
+++ strava.py
@@ -18,30 +18,10 @@ def fetch_strava_activities(
     ) -> pd.DataFrame:
     """Returns a Pandas DataFrame containing user strava activity data from the activities endpoint."""
 
-    # first, load access and refresh tokens:
-    token_data = fetch_strava_tokens()
-
-    # next up, set the access_token. 
-    # check if the most recently pulled token has expired. 
-    # refresh it and save locally if so, otherwise, use the current token
-    # and make a request to strava's activities endpoint:
-
-    if token_data["expires_at"] < time.time():
-        new_token_data = refresh_access_token(
-            client_id=client_id,
-            client_secret=client_secret,
-            refresh_token=token_data["refresh_token"]
-        )
-
-        save_strava_tokens(
-            token_file_path=token_file_path,
-            token_data=new_token_data
-        )
-
-        access_token = new_token_data["access_token"]
-
-    else:
-        access_token = token_data["access_token"]
+    access_token = access_token_workflow(
+        client_id=client_id,
+        client_secret=client_secret
+    )
 
     if access_token:
         response = requests.get(
@@ -87,10 +67,8 @@ def fetch_strava_activities(
                 all_activities['activity_begin_longitude'].append(activity["start_latlng"][1])
                 all_activities['activity_end_latitude'].append(activity["end_latlng"][0])
                 all_activities['activity_end_longitude'].append(activity["end_latlng"][1])
-
             
             return pd.DataFrame(data=all_activities, columns=[key for key in all_activities.keys()])
-        
 
         else:
             print(f"Error fetching data: {response.status_code}, {response.json()}")
@@ -99,34 +77,80 @@ def fetch_strava_activities(
         print("Could not get a valid access token.")
 
 
-def fetch_strava_tokens() -> Dict[str, Any]:
+def fetch_strava_athletes(
+        client_id:str,
+        client_secret:str
+    ) -> pd.DataFrame:
+    """Returns a Pandas DataFrame containing user strava athlete data from the athlete endpoint."""
+
+    access_token = access_token_workflow(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    if access_token:
+        response = requests.get(
+            url="https://www.strava.com/api/v3/athlete",
+            headers={"Authorization": f"Bearer {access_token}"}
+        )
+
+         # if our request is successful, parse the response and
+        # stage activity records as a dictionary for later use
+        if response.status_code == 200:
+            athlete = response.json()
+            user_dict = {
+                "athlete_id": [athlete["id"]],  #  this will need to be refactored when multiple athletes data is pulled in single run
+                "athlete_full_name": [athlete["firstname"]+" "+athlete["lastname"]]
+            }
+
+        return pd.DataFrame(data=user_dict, columns=[key for key in user_dict.keys()])
+
+def access_token_workflow(
+        client_id:str,
+        client_secret:str,
+    ) -> str:
+    """Returns a non-expired Strava Access token for API authentication."""
+
+    # first, load locally stored strava access and refresh tokens:
+    token_data = load_local_token_data()
+
+    # next up, set the access_token. 
+    # check if the most recently pulled token has expired. 
+    # refresh it and save locally if so, otherwise, use the current token
+    # and make a request to strava's activities endpoint:
+
+    if token_data["expires_at"] < time.time():
+        response = requests.post(
+            url="https://www.strava.com/oauth/token",
+            data={
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "grant_type": "refresh_token",
+                "refresh_token": token_data["refresh_token"]
+            }
+        )
+
+        new_token_data = response.json()
+
+        save_token_data_locally(
+            token_file_path=token_file_path,
+            token_data=new_token_data
+        )
+
+        return new_token_data["access_token"]
+
+    else:
+        return token_data["access_token"]
+
+
+def load_local_token_data() -> Dict[str, Any]:
     """Returns a JSON object containing Strava Access and Refresh tokens for API authentication."""
 
     with open(token_file_path, "r") as token_file:
         return json.load(token_file)
 
 
-def refresh_access_token(
-        client_id:str,
-        client_secret:str,
-        refresh_token:str
-    ) -> Dict[str, Any]:
-    """Returns a JSON object containing a non-expired Strava Access token."""
-
-    response = requests.post(
-        url="https://www.strava.com/oauth/token",
-        data={
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "grant_type": "refresh_token",
-            "refresh_token": refresh_token
-        }
-    )
-
-    return response.json()
-
-
-def save_strava_tokens(
+def save_token_data_locally(
         token_file_path:str,
         token_data:dict
     ):
@@ -150,12 +174,26 @@ def load_strava_data_to_postgres():
     )
     print("Successfully fetched strava activities data!")
 
-    print("Loading strava data to Postgres...")
+    print("Loading strava activities data to Postgres...")
     psql.load_dataframe_to_table(
         df=strava_df,
         destination_table="sb.strava_activities"
     )
-    print("Successfully loaded strava data to Postgres!")
+    print("Successfully loaded strava activities data to Postgres!")
+
+    print("Fetching strava athletes data...")
+    strava_df = fetch_strava_athletes(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    print("Successfully fetched strava athletes data!")
+
+    print("Loading strava athletes data to Postgres...")
+    psql.load_dataframe_to_table(
+        df=strava_df,
+        destination_table="sb.strava_athletes"
+    )
+    print("Successfully loaded strava athletes data to Postgres!")
 
 
 def fetch_secrets() -> Dict[str, Any]:
