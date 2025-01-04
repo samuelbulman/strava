6e4e770d544827e5a4c6b09e53e4ef39771121bf
samuelbulman <samuelabulman@gmail.com>
2025-01-04 16:57:37 -0600

    improvements to strava pipeline

--- strava.py
+++ strava.py
@@ -16,7 +16,7 @@ def fetch_strava_activities(
         client_id:str,
         client_secret:str
     ) -> pd.DataFrame:
-    """Returns a Pandas DataFrame containing user strava activity data from the activities endpoint."""
+    """Returns a Pandas DataFrame containing a strava athletes activity data from the activities endpoint(s)."""
 
     access_token = access_token_workflow(
         client_id=client_id,
@@ -24,57 +24,72 @@ def fetch_strava_activities(
     )
 
     if access_token:
-        response = requests.get(
-            url="https://www.strava.com/api/v3/athlete/activities",
-            headers={"Authorization": f"Bearer {access_token}"}
-        )
+        activities_data = strava_api_activities_response(access_token, detailed_activities=False)
+        activities = {
+            "activity_id": [],
+            "activity_name": [],
+            "athlete_id": [],
+            "activity_type": [],
+            "activity_timestamp": [],
+            "activity_distance": [],
+            "activity_duration_seconds": [],
+            "activity_elevation_high": [],
+            "activity_elevation_low": [],
+            "activity_avg_speed": [],
+            "activity_max_speed": [],
+            "calories_burned": [],
+        }
 
-        # if our request is successful, parse the response and
-        # stage activity records as a dictionary for later use
-        if response.status_code == 200:
-            activities_data = response.json()
-            all_activities = {
-                'activity_id': [],
-                'activity_name': [],
-                'athlete_id': [],
-                'activity_type': [],
-                'activity_timestamp': [],
-                'activity_distance': [],
-                'activity_duration_seconds': [],
-                'activity_elevation_high': [],
-                'activity_elevation_low': [],
-                'activity_avg_speed': [],
-                'activity_max_speed': [],
-                'activity_begin_latitude': [],
-                'activity_begin_longitude': [],
-                'activity_end_latitude': [],
-                'activity_end_longitude': []
-            }
-
-            for activity in activities_data:
-                all_activities['activity_id'].append(activity["id"])
-                all_activities['activity_name'].append(activity["name"])
-                all_activities['athlete_id'].append(activity["athlete"]["id"])
-                all_activities['activity_type'].append(activity["type"])
-                all_activities['activity_timestamp'].append(activity["start_date_local"])
-                all_activities['activity_distance'].append(activity["distance"])
-                all_activities['activity_duration_seconds'].append(activity["elapsed_time"])
-                all_activities['activity_elevation_high'].append(activity["elev_high"])
-                all_activities['activity_elevation_low'].append(activity["elev_low"])
-                all_activities['activity_avg_speed'].append(activity["average_speed"])
-                all_activities['activity_max_speed'].append(activity["max_speed"])
-                all_activities['activity_begin_latitude'].append(activity["start_latlng"][0])
-                all_activities['activity_begin_longitude'].append(activity["start_latlng"][1])
-                all_activities['activity_end_latitude'].append(activity["end_latlng"][0])
-                all_activities['activity_end_longitude'].append(activity["end_latlng"][1])
+        for activity in activities_data:
+            activities["activity_id"].append(activity.get("id"))
+            activities["activity_name"].append(activity.get("name"))
+            activities["athlete_id"].append(activity.get("athlete", {}).get("id", None))
+            activities["activity_type"].append(activity.get("type"))
+            activities["activity_timestamp"].append(activity.get("start_date_local"))
+            activities["activity_distance"].append(activity.get("distance"))
+            activities["activity_duration_seconds"].append(activity.get("elapsed_time"))
+            activities["activity_elevation_high"].append(activity.get("elev_high"))
+            activities["activity_elevation_low"].append(activity.get("elev_low"))
+            activities["activity_avg_speed"].append(activity.get("average_speed"))
+            activities["activity_max_speed"].append(activity.get("max_speed"))
+            # annoyingly, we have to hit a detailed activities endpoint to fetch how many calories were burned during a workout :roll-eyes:
+            detailed_activity_response = strava_api_activities_response(access_token, detailed_activities=True, activity_id=activity.get("id"))
+            activities["calories_burned"].append(detailed_activity_response.get("calories"))
+        
+        return pd.DataFrame(data=activities, columns=[key for key in activities.keys()])
             
-            return pd.DataFrame(data=all_activities, columns=[key for key in all_activities.keys()])
-
-        else:
-            print(f"Error fetching data: {response.status_code}, {response.json()}")
-    
     else:
         print("Could not get a valid access token.")
+    
+
+def strava_api_activities_response(
+        access_token:str,
+        detailed_activities:bool=False,
+        activity_id:int=None,
+    ) -> dict:
+    """
+    Makes a request to Strava's activities endpoint and returns a json response containing athlete activity data if successful.
+    
+    Parameters
+    ----------
+    detailed_activities (bool):
+    - When set to False, a request is made to return a list of all athelete activities (https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities).
+    - When set to True, a request is made to the activities endpoint for a specific activity_id, returning detailed information about that activity (https://developers.strava.com/docs/reference/#api-models-DetailedActivity).
+    """
+    
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    if detailed_activities:
+        url = f"https://www.strava.com/api/v3/activities/{activity_id}"
+    else:
+        url = "https://www.strava.com/api/v3/athlete/activities"
+
+    response = requests.get(url=url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data: {response.status_code}, {response.json()}")
 
 
 def fetch_strava_athletes(
@@ -104,6 +119,7 @@ def fetch_strava_athletes(
             }
 
         return pd.DataFrame(data=user_dict, columns=[key for key in user_dict.keys()])
+
 
 def access_token_workflow(
         client_id:str,
@@ -162,38 +178,43 @@ def save_token_data_locally(
         print("Tokens saved successfully!")
 
 
-def load_strava_data_to_postgres():
+def load_strava_data_to_postgres(
+        strava_client_id:str,
+        strava_client_secret:str
+    ):
     """Loads Strava source data to target Postgres table."""
 
     psql = Postgres()
 
-    print("Fetching strava activities data...")
+    print("Fetching strava activities data...\n")
     strava_df = fetch_strava_activities(
-        client_id=client_id,
-        client_secret=client_secret
+        client_id=strava_client_id,
+        client_secret=strava_client_secret
     )
-    print("Successfully fetched strava activities data!")
+    print("Successfully fetched strava activities data!\n")
 
-    print("Loading strava activities data to Postgres...")
+    print("Loading strava activities data to Postgres...\n")
     psql.load_dataframe_to_table(
         df=strava_df,
-        destination_table="sb.strava_activities"
+        schema="strava",
+        table="strava_activities"
     )
-    print("Successfully loaded strava activities data to Postgres!")
+    print("\nSuccessfully loaded strava activities data to Postgres!\n")
 
-    print("Fetching strava athletes data...")
+    print("Fetching strava athletes data...\n")
     strava_df = fetch_strava_athletes(
-        client_id=client_id,
-        client_secret=client_secret
+        client_id=strava_client_id,
+        client_secret=strava_client_secret
     )
-    print("Successfully fetched strava athletes data!")
+    print("Successfully fetched strava athletes data!\n")
 
-    print("Loading strava athletes data to Postgres...")
+    print("Loading strava athletes data to Postgres...\n")
     psql.load_dataframe_to_table(
         df=strava_df,
-        destination_table="sb.strava_athletes"
+        schema="strava",
+        table="strava_athletes"
     )
-    print("Successfully loaded strava athletes data to Postgres!")
+    print("\nSuccessfully loaded strava athletes data to Postgres!\n")
 
 
 def fetch_secrets() -> Dict[str, Any]:
@@ -207,7 +228,10 @@ def fetch_secrets() -> Dict[str, Any]:
 
 if __name__ == "__main__":
     secrets = fetch_secrets()
-    client_id = secrets["client_id"]
-    client_secret = secrets["client_secret"]
+    strava_client_id = secrets["client_id"]
+    strava_client_secret = secrets["client_secret"]
     token_file_path = secrets["token_file_path"]
-    load_strava_data_to_postgres()
+    load_strava_data_to_postgres(
+      strava_client_id=strava_client_id,
+      strava_client_secret=strava_client_secret
+    )
