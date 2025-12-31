351fc2afb74fa40f242be3be22b8cef13bb83fb6
samuelbulman <samuelabulman@gmail.com>
2025-12-31 14:55:21 -0600

    bug: calendar end date

--- dbt/models/sources/utilities/calendar.sql
+++ dbt/models/sources/utilities/calendar.sql
@@ -4,7 +4,7 @@ with date_spine as (
         dbt_utils.date_spine(
             datepart="day",
             start_date="'2022-01-01'::date",
-            end_date="(date_trunc('year', current_date) + interval '12 months' - interval '1 day')::date"
+            end_date="(date_trunc('year', current_date) + interval '12 months')::date"
         )
     }}
 )
